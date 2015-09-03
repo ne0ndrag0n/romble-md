bb304d61d5489f17612922efb13f828a0c3565fa
mipani <mipani@users.noreply.github.com>
2015-09-03 16:50:12 -0400

    this bad?

--- src/simpletextview.c
+++ src/simpletextview.c
@@ -8,14 +8,14 @@ SimpleTextView_vtable SimpleTextView_table = { SimpleTextView_ctor, SimpleTextVi
 void SimpleTextView_ctor( SimpleTextView* this, char* text, u8 x, u8 y ) {
 	u8 width = strlen( text );
 	
-	BaseView_ctor( &this->super, x, y, width, 1 );
+	BaseView_ctor( ( BaseView* ) this, x, y, width, 1 );
 
 	// Fix the incompatible pointer types warning
 	this->super.functions = &SimpleTextView_table;
 }
 
 void SimpleTextView_render( SimpleTextView* this ) {
-	BaseView_render( &this->super );
+	BaseView_render( ( BaseView* ) this );
 
 	VDP_drawText( this->text, this->super.absX, this->super.absY );
 }
