70545334073aeee963ef77f1f6fd5e4fdace7cd5
mipani <mipani@users.noreply.github.com>
2015-11-04 16:28:26 -0500

    Revised version of bit trie using two bits at a time

--- src/bit_trie.c
+++ src/bit_trie.c
@@ -9,37 +9,13 @@
 #include <types.h>
 #include <stddef.h>
 #include <res/globals.h>
+#include <romble.h>
 
-void BitwiseTrieNode_ctor( BitwiseTrieNode* this, bool isLeafNode ) {
-	if( isLeafNode == TRUE ) {
-		this->leftChild.data = NULL;
-		this->rightChild.data = NULL;
-	} else {
-		this->leftChild.node = NULL;
-		this->rightChild.node = NULL;
-	}
+void BitwiseTrieNode_ctor( BitwiseTrieNode* this ) {
+	// Allocate four child elements; they will be zeroed out
+	this->children = Romble_alloc_d( sizeof( BitwiseTrieNode ) * 4, TRUE, FILE_LINE() );
 }
 
 void BitwiseTrieNode_dtor( BitwiseTrieNode* this ) {
-
-}
-
-void BitwiseTrieNode_insert( BitwiseTrieNode* this, u8 key, void* value ) {
-	BitwiseTrieNode* current = this;
-
-	for( size_t i = 0; i != 8; i++ ) {
-		if( ( ( key >> i ) & 0x01 ) == 1 ) {
-			// Use right child
-		} else {
-			// Use left child
-		}
-	}
-}
-
-void BitwiseTrieNode_delete( BitwiseTrieNode* this, u8 key ) {
-
-}
-
-void* BitwiseTrieNode_get( BitwiseTrieNode* this, u8 key ) {
-
+	// Recursively remove all elements
 }
--- src/bit_trie.h
+++ src/bit_trie.h
@@ -13,27 +13,12 @@
 #ifndef		BIT_TRIE
 #define		BIT_TRIE
 
-struct BitwiseTrieNode;
-
-typedef union BitwiseTrieNodeChild {
+typedef union BitwiseTrieNode {
 	void* data;
-	struct BitwiseTrieNode* node;
-} BitwiseTrieNodeChild;
-
-typedef struct BitwiseTrieNode {
-	/**
-	 * Left holds 0, right holds 1
-	 */
-	BitwiseTrieNodeChild leftChild;
-	BitwiseTrieNodeChild rightChild;
+	union BitwiseTrieNode* children;
 } BitwiseTrieNode;
 
-void BitwiseTrieNode_ctor( BitwiseTrieNode* this, bool isLeafNode );
+void BitwiseTrieNode_ctor( BitwiseTrieNode* this );
 void BitwiseTrieNode_dtor( BitwiseTrieNode* this );
-
-void BitwiseTrieNode_insert( BitwiseTrieNode* this, u8 key, void* value );
-void BitwiseTrieNode_delete( BitwiseTrieNode* this, u8 key );
-
-void* BitwiseTrieNode_get( BitwiseTrieNode* this, u8 key );
 
 #endif
