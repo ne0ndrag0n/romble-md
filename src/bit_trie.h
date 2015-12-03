0f1f1d74cd8bce6bb53df525e3bf5cb208243192
mipani <mipani@users.noreply.github.com>
2015-12-03 18:35:14 -0500

    Revert "BitwiseTrie"
    This reverts commit 69c62bde74efa8d3601644abbdf5558ae1856741.

--- src/bit_trie.h
+++ src/bit_trie.h
@@ -12,7 +12,6 @@
 #include <types.h>
 #include <stddef.h>
 #include <res/globals.h>
-#include <sizedarray.h>
 
 #define	 BitwiseTrieNode_CELL_MASK	0x03
 
@@ -20,11 +19,6 @@ typedef union BitwiseTrieNode {
 	void* data;
 	union BitwiseTrieNode** children;
 } BitwiseTrieNode;
-
-typedef struct BitwiseTrie {
-	BitwiseTrieNode* root;
-	SizedArray keys;
-} BitwiseTrie;
 
 void BitwiseTrieNode_ctor( BitwiseTrieNode* this, bool leaf );
 void BitwiseTrieNode_dtor( BitwiseTrieNode* this );
@@ -36,14 +30,4 @@ void BitwiseTrieNode_delete( BitwiseTrieNode* this, u8 key );
 
 void BitwiseTrieNode_deleteHelper( BitwiseTrieNode* current, BitwiseTrieNode* parent, u8 key, u8 step );
 extern u8 BitwiseTrieNode_getCell( u8 key, u8 step );
-
-// ------- BitwiseTrie container (wraps a BitwiseTrieNode) -------
-
-void BitwiseTrie_ctor( BitwiseTrie* this );
-void BitwiseTrie_dtor( BitwiseTrie* this );
-
-void BitwiseTrie_insert( BitwiseTrie* this, u8 key, void* data );
-void* BitwiseTrie_get( BitwiseTrie* this, u8 key );
-void BitwiseTrie_delete( BitwiseTrie* this, u8 key );
-
 #endif
