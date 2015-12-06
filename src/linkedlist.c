6bcd233eb967f2b7f30a1172a43999093366b4b6
mipani <mipani@users.noreply.github.com>
2015-12-05 23:49:47 -0500

    Linked list object and methods

---
+++ src/linkedlist.c
@@ -0,0 +1,45 @@
+#include <linkedlist.h>
+#include <romble.h>
+#include <stdlib.h>
+
+void LinkedListNode_ctor( LinkedListNode* this ) {
+	this->data = NULL;
+	this->next = NULL;
+}
+
+void LinkedListNode_dtor( LinkedListNode* this ) {
+
+	if( this->next != NULL ) {
+		LinkedListNode_dtor( this->next );
+	}
+
+	free( this );
+}
+
+void LinkedListNode_insertEnd( LinkedListNode* this, void* data ) {
+	LinkedListNode* current = this;
+
+	while( current->next != NULL ) {
+		current = current->next;
+	}
+
+	current->next = Romble_alloc_d( sizeof( LinkedListNode ), TRUE, FILE_LINE() );
+	LinkedListNode_ctor( current->next );
+
+	current->data = data;
+}
+
+void* LinkedListNode_findData( LinkedListNode* this, LinkedListNode_SearchPredicate predicateFunction ) {
+	LinkedListNode* current = this;
+
+	while( current != NULL ) {
+		if( predicateFunction( current->data ) == TRUE ) {
+			return current->data;
+		}
+
+		current = current->next;
+	}
+
+	// Nothing was found
+	return NULL;
+}
---
+++ src/linkedlist.h
@@ -0,0 +1,47 @@
+#ifndef LINKED_LIST
+#define LINKED_LIST
+
+#include <res/globals.h>
+#include <types.h>
+
+struct LinkedListNode;
+
+typedef struct LinkedListNode {
+	void* data;
+	struct LinkedListNode* next;
+} LinkedListNode;
+
+typedef bool ( *LinkedListNode_SearchPredicate )( void* );
+
+void LinkedListNode_ctor( LinkedListNode* this );
+void LinkedListNode_dtor( LinkedListNode* this );
+
+/**
+ * Inserts at the end of the linked list (last node pointing to NULL)
+ *
+ * @param		{void*}		data		The data to be inserted into the linked list. Your container
+ * 										method should keep track of the type of this data.
+ */
+void LinkedListNode_insertEnd( LinkedListNode* this, void* data );
+
+/**
+ * Find and return data in the list based on given criteria. Data will be returned from the first node
+ * in which predicateFunction returns TRUE.
+ *
+ * @param		{LinkedListNode_SearchPredicate}		predicateFunction		This function shall return TRUE to signify the data has been found.
+ *
+ * @returns		{void*|NULL}													void* if data is found, NULL if data is not found.
+ */
+void* LinkedListNode_findData( LinkedListNode* this, LinkedListNode_SearchPredicate predicateFunction );
+
+/**
+ * Remove data in the linked list based on given criteria. The node causing predicateFunction to return TRUE
+ * will be removed from the list. If removeAll is TRUE, the remove function will continue iterating through
+ * the linked list until the final node is reached.
+ *
+ * @param		{LinkedListNode_SearchPredicate}		predicateFunction		This function shall return TRUE to signify the data has been found.
+ * @param		{bool}									removeAll				If TRUE, continue to remove after the first removal.
+ */
+void LinkedListNode_remove( LinkedListNode* this, LinkedListNode_SearchPredicate predicateFunction, bool removeAll );
+
+#endif
